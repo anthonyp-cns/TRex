be91a7b28956c79c69ce84ae180262ef262dacb7
Anthony Polsinelli <anthonyp@carbonnetworksolutions.com>
2025-07-07 17:47:30 -0400

    Update Tests

--- run_me.py
+++ run_me.py
@@ -23,15 +23,15 @@ devices = [
 ]
 
 v4_tests =[
-    {"name": "1101_tcp_64b","src_range": "198.18.104.","dst_range": "203.0.113.","packet_size": 64, "num_flows":240,
-     "pg_id":10, "vlan_id": 1101, 'protocol': "tcp"},
-    # {"name": "1101_tcp_512b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 512, "num_flows": 240,
-    #  "pg_id": 10, "vlan_id": 1101, 'protocol': "tcp"},
-    # {"name": "1101_tcp_1500b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 1500, "num_flows": 240,
-    #  "pg_id": 10, "vlan_id": 1101, 'protocol': "tcp"},
-    {"name": "1101_udp_64b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 64, "num_flows": 240,
-     "pg_id": 10, "vlan_id": 1101, 'protocol': "udp"},
-    # {"name": "1101_udp_512b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 512, "num_flows": 240,
+    # {"name": "1101_tcp_64b","src_range": "198.18.104.","dst_range": "203.0.113.","packet_size": 64, "num_flows":240,
+    #  "pg_id":10, "vlan_id": 1101, 'protocol': "tcp"},
+    # # {"name": "1101_tcp_512b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 512, "num_flows": 240,
+    # #  "pg_id": 10, "vlan_id": 1101, 'protocol': "tcp"},
+    # # {"name": "1101_tcp_1500b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 1500, "num_flows": 240,
+    # #  "pg_id": 10, "vlan_id": 1101, 'protocol': "tcp"},
+    # {"name": "1101_udp_64b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 64, "num_flows": 240,
+    #  "pg_id": 10, "vlan_id": 1101, 'protocol': "udp"},
+    # # {"name": "1101_udp_512b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 512, "num_flows": 240,
     #  "pg_id": 10, "vlan_id": 1101, 'protocol': "udp"},
     # {"name": "1101_udp_1500b", "src_range": "198.18.104.", "dst_range": "203.0.113.", "packet_size": 1500, "num_flows": 240,
     #  "pg_id": 10, "vlan_id": 1101, 'protocol': "udp"},
@@ -49,8 +49,8 @@ v4_tests =[
     #  "pg_id": 10, "vlan_id": 1201, 'protocol': "udp"}
 ]
 v4_imix_tests = [
-    {"name": "1101_v4_imix","src_range": "198.18.104.0/24","dst_range": "203.0.113.0/24", "num_flows":240,
-     "pg_id":10, "vlan_id": 1101, 'protocol': "tcp"},
+    # {"name": "1101_v4_imix","src_range": "198.18.104.0/24","dst_range": "203.0.113.0/24", "num_flows":240,
+    #  "pg_id":10, "vlan_id": 1101, 'protocol': "tcp"},
    # {"name": "1201_v4_imix", "src_range": "100.65.0.0/24", "dst_range": "203.0.113.0/24", "num_flows": 240,
    #  "pg_id": 10, "vlan_id": 1301, 'protocol': "udp"}
 ]

7f6bb2650240af03964956aaa11ec16276e35275
Anthony Polsinelli <anthonyp@carbonnetworksolutions.com>
2025-07-07 15:48:08 -0400

    Update Tests

--- Stateful/Create_v4_stream.py
+++ Stateful/Create_v4_stream.py
@@ -18,13 +18,19 @@ class StatefulTCPClient:
         server_prog.send(b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello")
 
         return ASTFProfile(
-            cap_list=[ASTFCapInfo(l7_proto="http", c_prog=client_prog, s_prog=server_prog)],
+            cap_list=[
+                ASTFCapInfo(
+                    c_prog=client_prog,
+                    s_prog=server_prog
+                )
+            ],
             ip_gen=ASTFIPGen(
                 dist_client=ASTFIPGenDist(ip_start=self.src_range + "1", ip_end=self.src_range + "254"),
                 dist_server=ASTFIPGenDist(ip_start=self.dst_range + "1", ip_end=self.dst_range + "254"),
             ),
             vlan=self.vlan_id
         )
+
 
 def register():
     return StatefulTCPClient("198.18.104.", "203.0.113.").get_profile()
